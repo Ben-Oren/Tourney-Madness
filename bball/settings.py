1674e51f455f832edb61804e48d3b085045e9b8c
Ben-Oren <oren.ben.k@gmail.com>
2018-07-25 15:25:23 -0500

    Create settings.py

---
+++ bball/settings.py
@@ -0,0 +1,7 @@
+BOT_NAME = 'playerscraping'
+
+SPIDER_MODULES = ['playerscraping.spiders']
+NEWSPIDER_MODULE = 'playerscraping.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'playerscraping (+http://www.yourdomain.com)'
